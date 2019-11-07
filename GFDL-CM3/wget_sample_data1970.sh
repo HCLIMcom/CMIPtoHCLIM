98f91646ef2222d806042946ee9bb9e10a769b70
doblerone <andreasd@met.no>
2019-11-07 09:52:23 +0100

    Update wget_sample_data1970.sh
    Changed file path

--- GFDL-CM3/wget_sample_data1970.sh
+++ GFDL-CM3/wget_sample_data1970.sh
@@ -1,16 +1,16 @@
 #Download a set of sample data for the GFDL-CM3toGRIB.sh script
 #Produced from ESGF data
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/va_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ua_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ts_6hr_GFDL-CM3_historical_r1i1p1_1970.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/tos_6hr_GFDL-CM3_historical_r1i1p1_1970.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ta_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/sic_6hr_GFDL-CM3_historical_r1i1p1_1970.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/sftlf_fx_GFDL-CM3_historical_r0i0p0.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ps_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/orog_fx_GFDL-CM3_historical_r0i0p0.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/hus_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
-wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ERAI-surfacefields-global-climatology-1990-2005.GFDLgrid.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/va_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ua_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ts_6hr_GFDL-CM3_historical_r1i1p1_1970.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/tos_6hr_GFDL-CM3_historical_r1i1p1_1970.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ta_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/sic_6hr_GFDL-CM3_historical_r1i1p1_1970.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/sftlf_fx_GFDL-CM3_historical_r0i0p0.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ps_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/orog_fx_GFDL-CM3_historical_r0i0p0.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/hus_6hrLev_GFDL-CM3_historical_r1i1p1_1970.nc
+wget http://thredds.met.no/thredds/fileServer/metusers/andreasd/HCLIM38/HCLIM38-ALADIN_GFDL-CM3_samples/forcing_files/ERAI-surfacefields-global-climatology-1990-2005.GFDLgrid.nc
 
 
  
